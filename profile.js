840446a20403ac46154c19e58caa36c41a7ca13e
Mohammed Adwan <55084310+el7ommed@users.noreply.github.com>
2024-12-07 21:00:46 +0300

    fetch audit data

--- profile.js
+++ profile.js
@@ -2,6 +2,13 @@ console.log("profile.js loaded!");
 
 import { jwt } from './redirect.js';
 
+let currUser = {
+	id: 0,
+	login: '',
+	firstName: '',
+	lastName: ''
+};
+
 const headers = {
 	'Authorization': 'Bearer ' + jwt,
 	'Content-Type': 'application/json',
@@ -11,8 +18,31 @@ const headers = {
 };
 
 const basicInfo = JSON.stringify({
-	query: "query basicInfo { user { id login firstName lastName }}",
+	query: `query basicInfo {
+		user {
+			id
+			login
+			firstName
+			lastName
+		}
+	}`,
 	operationName: "basicInfo"
+});
+
+const lastAuditsGiven = JSON.stringify({
+	query: `query lastAuditsGiven { 
+			audit(where: {auditor: {id: {_eq: ${currUser.id}}}}, order_by: {id: desc}, limit: 5) { 
+				createdAt 
+				group { 
+					captain { 
+						firstName 
+						lastName 
+						login 
+					} 
+				} 
+			} 
+		}`,
+	operationName: "lastAuditsGiven"
 });
 
 fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
@@ -21,23 +51,40 @@ fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
 	body: basicInfo
 })
 	.then(response => response.json())
-	.then(data => {
-		if (data.errors) {
-			console.error('GraphQL Error:', data.errors);
+	.then(userdata => {
+		if (userdata.errors) {
+			console.error('GraphQL Error:', userdata.errors);
 		} else {
-			const user = data.data.user[0];
+			currUser = userdata.data.user[0];
 
-			document.title = `${user.firstName} ${user.lastName}'s Profile`;
+			document.title = `${currUser.firstName} ${currUser.lastName}'s Profile`;
 
 			const navbarBrand = document.querySelector('.navbar-brand');
 			if (navbarBrand) {
-				navbarBrand.textContent = `Hello, ${user.firstName} ${user.lastName} (${user.login})`;
+				navbarBrand.textContent = `Hello, ${currUser.firstName} ${currUser.lastName} (${currUser.login})`;
 			}
 
 			const navUserID = document.getElementById('nav-user-id');
 			if (navUserID) {
-				navUserID.textContent = "user #" + user.id;
+				navUserID.textContent = "user #" + currUser.id;
 			}
+		}
+	})
+	.catch(error => {
+		console.error('Error:', error);
+	});
+
+fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
+	method: 'POST',
+	headers: headers,
+	body: lastAuditsGiven
+})
+	.then(response => response.json())
+	.then(auditdata => {
+		if (auditdata.errors) {
+			console.error('GraphQL Error:', auditdata.errors);
+		} else {
+			console.log(auditdata);
 		}
 	})
 	.catch(error => {
