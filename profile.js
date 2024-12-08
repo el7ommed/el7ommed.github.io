1cda1c071e2ab9bd98a3d62d7ca029c1e72e7670
Mohammed Adwan <55084310+el7ommed@users.noreply.github.com>
2024-12-08 20:11:57 +0300

    avoid empty slice jwt

--- profile.js
+++ profile.js
@@ -1,38 +1,38 @@
 console.log("profile.js loaded!");
 
-import { jwt } from './redirect.js';
+import { jwt } from "./redirect.js";
 
 let currUser = {
 	id: 0,
-	login: '',
-	firstName: '',
-	lastName: '',
+	login: "",
+	firstName: "",
+	lastName: "",
 };
 
 const headers = {
-	'Authorization': 'Bearer ' + jwt,
-	'Content-Type': 'application/json',
-	'Accept': '*/*',
-	'Cache-Control': 'no-cache',
-	'Pragma': 'no-cache',
+	"Authorization": "Bearer " + jwt,
+	"Content-Type": "application/json",
+	"Accept": "*/*",
+	"Cache-Control": "no-cache",
+	"Pragma": "no-cache",
 };
 
 async function fetchBasicInfo() {
 	const basicInfo = JSON.stringify({
 		query:
-		`query basicInfo {
-			user {
-				id
-				login
-				firstName
-				lastName
-			}
-		}`,
+			`query basicInfo {
+				user {
+					id
+					login
+					firstName
+					lastName
+				}
+			}`,
 		operationName: "basicInfo"
 	});
 
-	const response = await fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
-		method: 'POST',
+	const response = await fetch("https://learn.reboot01.com/api/graphql-engine/v1/graphql", {
+		method: "POST",
 		headers: headers,
 		body: basicInfo,
 	});
@@ -49,37 +49,37 @@ async function fetchBasicInfo() {
 function updatePage(user) {
 	document.title = `${user.firstName} ${user.lastName}'s Profile`;
 
-	const navbarBrand = document.querySelector('.navbar-brand');
+	const navbarBrand = document.querySelector(".navbar-brand");
 	if (navbarBrand) {
 		navbarBrand.textContent = `Hello, ${user.firstName} ${user.lastName} (${user.login})`;
 	}
 
-	const navUserID = document.getElementById('nav-user-id');
+	const navUserID = document.getElementById("nav-user-id");
 	if (navUserID) {
-		navUserID.textContent = 'user #' + user.id;
+		navUserID.textContent = "user #" + user.id;
 	}
 }
 
 async function fetchLastAudits() {
 	const lastAuditsGiven = JSON.stringify({
 		query:
-		`query lastAuditsGiven {
-			audit(where: {auditor: {id: {_eq: ${currUser.id}}}}, order_by: {id: desc}, limit: 5) {
-				createdAt
-				group {
-					captain {
-						firstName
-						lastName
-						login
+			`query lastAuditsGiven {
+				audit(where: {auditor: {id: {_eq: ${currUser.id}}}}, order_by: {id: desc}, limit: 5) {
+					createdAt
+					group {
+						captain {
+							firstName
+							lastName
+							login
+						}
 					}
 				}
-			}
-		}`,
+			}`,
 		operationName: "lastAuditsGiven",
 	});
 
-	const response = await fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
-		method: 'POST',
+	const response = await fetch("https://learn.reboot01.com/api/graphql-engine/v1/graphql", {
+		method: "POST",
 		headers: headers,
 		body: lastAuditsGiven,
 	});
@@ -93,17 +93,17 @@ async function fetchLastAudits() {
 async function fetchTopProjects() {
 	const topProjects = JSON.stringify({
 		query:
-		`query topProjects {
-			xp_view(limit: 5, order_by: {amount: desc}) {
-				amount
-				path
-			}
-		}`,
+			`query topProjects {
+				xp_view(limit: 5, order_by: {amount: desc}) {
+					amount
+					path
+				}
+			}`,
 		operationName: "topProjects"
 	});
 
-	const response = await fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql', {
-		method: 'POST',
+	const response = await fetch("https://learn.reboot01.com/api/graphql-engine/v1/graphql", {
+		method: "POST",
 		headers: headers,
 		body: topProjects,
 	});
@@ -125,10 +125,10 @@ async function fetchTopProjects() {
 	}
 })();
 
-const logoutButton = document.getElementById('Logout');
+const logoutButton = document.getElementById("Logout");
 if (logoutButton) {
-	logoutButton.addEventListener('click', () => {
-		localStorage.removeItem('jwt');
-		window.location.href = 'index.html';
+	logoutButton.addEventListener("click", () => {
+		localStorage.removeItem("jwt");
+		window.location.href = "index.html";
 	});
 }
