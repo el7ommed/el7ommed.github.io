950aaa18cf52e3665668ec24d788e9414bc2adfc
Mohammed Adwan <55084310+el7ommed@users.noreply.github.com>
2024-12-09 20:24:21 +0300

    populate skills cards

--- profile.js
+++ profile.js
@@ -141,6 +141,7 @@ async function fetchTopSkills() {
 			}
 		}`
 	const data = await fetchGraphQL(query, "highestSkills");
+	populateSkills(data.user[0].transactions);
 	// console.log("Top Skills:", data.user[0].transactions);
 }
 
@@ -192,6 +193,24 @@ function populateProjects(projects) {
 				<!-- <div class="card-footer text-muted">Description</div> -->
 			</div>`;
 		projectsContainer.appendChild(card);
+	});
+}
+
+const skillsContainer = document.getElementById("skills-container");
+function populateSkills(transactions) {
+	skillsContainer.innerHTML = "";
+	transactions.forEach((transaction) => {
+		// console.log(transaction);
+		const card = document.createElement("div");
+		card.className = "col";
+		card.innerHTML =
+			`<div class="card">
+				<div class="card-header">${transaction.type.split('_').pop()}</div>
+				<div class="card-body">
+					<h5 class="card-title">${transaction.amount}</h5>
+				</div>
+			</div>`;
+		skillsContainer.appendChild(card);
 	});
 }
 
